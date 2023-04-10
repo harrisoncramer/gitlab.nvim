22f0a46ddccfa485d9e745af8ce44de2dc5ce672
Harrison Cramer <harrisoncramer@gmail.com>
2023-04-09 22:07:39 -0400

    Successful POST of new comment 🚀

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"encoding/json"
 	"errors"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"log"
 	"mime/multipart"
@@ -15,6 +14,32 @@ import (
 	"strings"
 	"time"
 )
+
+const (
+	projectId      = 40444811
+	discussionsUrl = "https://gitlab.com/api/v4/projects/%d/merge_requests/%s/discussions"
+	mrVersionsUrl  = "https://gitlab.com/api/v4/projects/%d/merge_requests/%s/versions"
+)
+
+type Comment struct {
+	View                    string `json:"view"`
+	LineType                string `json:"line_type"`
+	MergeRequestDiffHeadSha string `json:"merge_request_diff_head_sha"`
+	InReplyToDiscussionId   string `json:"in_reply_to_discussion_id"`
+	NoteProjectId           string `json:"note_project_id"`
+	TargetType              string `json:"target_type"`
+	TargetId                int    `json:"target_id"`
+	ReturnDiscussion        bool   `json:"return_discussion"`
+	Note                    struct {
+		Note         string `json:"note"`
+		Position     string `json:"position"`
+		NoteableType string `json:"noteable_type"`
+		NoteableId   int    `json:"noteable_id"`
+		CommitId     int    `json:"commit_id"`
+		Type         string `json:"type"`
+		LineCode     string `json:"line_code"`
+	}
+}
 
 type Position struct {
 	BaseSha      string    `json:"base_sha"`
@@ -51,39 +76,9 @@ type MRVersion struct {
 	RealSize       string    `json:"real_size"`
 }
 
-const (
-	projectId = 40444811
-)
-
-func getCurrentMergeId() string {
-	gitCmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
-
-	output, err := gitCmd.Output()
-	if err != nil {
-		fmt.Println("Error running git rev-parse:", err)
-		os.Exit(1)
-	}
-
-	sourceBranch := strings.TrimSpace(string(output))
-
-	glabCmd := exec.Command("bash", "-c", `glab mr list --source-branch=`+sourceBranch+` | cat | head -n3 | tail -n 1 | awk '{print $1}' | cut -c 2`)
-
-	output, err = glabCmd.Output()
-	if err != nil {
-		fmt.Println("Error running the command:", err)
-		os.Exit(1)
-	}
-
-	result := strings.TrimSpace(string(output))
-
-	return result
-
-}
-
 func main() {
 	mergeId := getCurrentMergeId()
 
-	/* Get MR information */
 	err, response := getMRVersions(mergeId, projectId)
 	if err != nil {
 		log.Fatalf("Error making diff thread: %err", err)
@@ -100,80 +95,60 @@ func main() {
 	}
 
 	/* Create a thread for discussion with latest MR information */
-	createComment(mergeId, projectId, diffVersionInfo[0])
+	err = createComment(mergeId, projectId, diffVersionInfo[0])
+	if err != nil {
+		log.Fatalf("Error making comment thread: %v", err)
+	}
+
+	log.Println("Comment created!")
 
 }
 
-func createComment(mergeId string, projectId int, mrInfo MRVersion) {
+/* POSTs the comment to the merge request */
+func createComment(mergeId string, projectId int, mrInfo MRVersion) error {
+
 	payload := &bytes.Buffer{}
 	writer := multipart.NewWriter(payload)
-
-	makeNote(mrInfo, writer, "This is some comment")
-
+	_ = writer.WriteField("position[position_type]", "text")
+	_ = writer.WriteField("position[base_sha]", mrInfo.BaseCommitSHA)
+	_ = writer.WriteField("position[head_sha]", mrInfo.HeadCommitSHA)
+	_ = writer.WriteField("position[start_sha]", mrInfo.StartCommitSHA)
+	_ = writer.WriteField("position[new_path]", "main.go")
+	_ = writer.WriteField("position[old_path]", "main.go")
+	_ = writer.WriteField("position[new_line]", "119")
+	_ = writer.WriteField("body", "Another new comment!")
 	err := writer.Close()
 	if err != nil {
-		log.Fatalf("Error closing writer: %v", err)
+		return err
 	}
 
 	client := &http.Client{}
-	url := fmt.Sprintf("https://gitlab.com/api/v4/projects/%d/merge_requests/%s/discussions", projectId, mergeId)
+	req, err := http.NewRequest(http.MethodPost, fmt.Sprintf(discussionsUrl, projectId, mergeId), payload)
 
-	req, err := http.NewRequest(http.MethodPost, url, payload)
 	if err != nil {
-		log.Fatalf("Error creating new request: %v", err)
+		return err
 	}
-
 	req.Header.Add("PRIVATE-TOKEN", os.Getenv("GITLAB_TOKEN"))
-	req.Header.Set("Content-Type", writer.FormDataContentType())
 
+	req.Header.Set("Content-Type", writer.FormDataContentType())
 	res, err := client.Do(req)
 	if err != nil {
-		log.Fatalf("Failed to post comment: %s", err)
+		return err
 	}
-
-	_, err = io.ReadAll(res.Body)
-	if err != nil {
-		log.Fatalf("Error reading body: %v", err)
-	}
-
-	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		log.Fatalf("Recieved non-200 exit: %d", res.StatusCode)
-		log.Fatalf("Error: %s", res.Body)
+		return err
 	}
+	defer res.Body.Close()
 
-	log.Println("Success!")
+	return nil
 }
-
-// func createThread(mergeId string, projectId int) {
-//
-// 	url := fmt.Sprintf("https://gitlab.com/api/v4/projects/%d/merge_requests/%s/discussions?body=comment", projectId, mergeId)
-// 	req, err := http.NewRequest(http.MethodPost, url, nil)
-// 	if err != nil {
-// 		log.Fatalf("Error creating thread request: %v", err)
-// 	}
-//
-// 	req.Header.Add("PRIVATE-TOKEN", os.Getenv("GITLAB_TOKEN"))
-//
-// 	client := &http.Client{}
-// 	res, err := client.Do(req)
-// 	if err != nil {
-// 		log.Fatalf("Error creating thread: %v", err)
-// 	}
-//
-// 	defer res.Body.Close()
-// 	if res.StatusCode != http.StatusOK {
-// 		log.Fatalf("Error creating thread: %v", err)
-// 	}
-//
-// }
 
 /* Gets the latest merge request revision data */
 func getMRVersions(mergeId string, projectId int) (e error, response *http.Response) {
 
 	gitlabToken := os.Getenv("GITLAB_TOKEN")
-	url := fmt.Sprintf("https://gitlab.com/api/v4/projects/%d/merge_requests/%s/versions", projectId, mergeId)
+	url := fmt.Sprintf(mrVersionsUrl, projectId, mergeId)
 
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 
@@ -197,19 +172,28 @@ func getMRVersions(mergeId string, projectId int) (e error, response *http.Respo
 	return nil, response
 }
 
-/* Makes a note on the MR */
-func makeNote(version MRVersion, w *multipart.Writer, comment string) error {
-	payload := &bytes.Buffer{}
+/* Gets the current merge request ID from local Git */
+func getCurrentMergeId() string {
+	gitCmd := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD")
 
-	writer := multipart.NewWriter(payload) /* Creates a new multipart writer, which is a MIME type with multiple parts */
-	_ = writer.WriteField("position[position_type]", "text")
-	_ = writer.WriteField("position[base_sha]", version.BaseCommitSHA)
-	_ = writer.WriteField("position[head_sha]", version.HeadCommitSHA)
-	_ = writer.WriteField("position[start_sha]", version.StartCommitSHA)
-	_ = writer.WriteField("position[new_path]", "New_file.txt")
-	_ = writer.WriteField("position[old_path]", "New_file.txt")
-	_ = writer.WriteField("position[new_line]", "1")
-	_ = writer.WriteField("body", comment)
+	output, err := gitCmd.Output()
+	if err != nil {
+		fmt.Println("Error running git rev-parse:", err)
+		os.Exit(1)
+	}
 
-	return nil
+	sourceBranch := strings.TrimSpace(string(output))
+
+	glabCmd := exec.Command("bash", "-c", `glab mr list --source-branch=`+sourceBranch+` | cat | head -n3 | tail -n 1 | awk '{print $1}' | cut -c 2`)
+
+	output, err = glabCmd.Output()
+	if err != nil {
+		fmt.Println("Error running the command:", err)
+		os.Exit(1)
+	}
+
+	result := strings.TrimSpace(string(output))
+
+	return result
+
 }
