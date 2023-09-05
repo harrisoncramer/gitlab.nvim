6d4660b28b3855c50cdbb4021e24f9595026f232
mrparalon <mr.paralon@gmail.com>
2023-09-05 11:16:01 +0400

    Deleted prints, add more nil checks

--- lua/gitlab/reviewer/diffview.lua
+++ lua/gitlab/reviewer/diffview.lua
@@ -12,18 +12,24 @@ local M        = {
 -- when the reviewer is consumed by other code. They must follow the specification
 -- outlined in the reviewer/init.lua file
 M.open         = function()
-    print("OPENING diffview")
     vim.api.nvim_command(string.format("DiffviewOpen %s", state.INFO.target_branch))
     M.tabnr = vim.api.nvim_get_current_tabpage()
 end
 
 M.jump         = function(file_name, new_line, old_line)
+    if M.tabnr == nil then
+        vim.notify("Can't jump to Diffvew. Is it open?", vim.log.levels.ERROR)
+        return
+    end
     vim.api.nvim_set_current_tabpage(M.tabnr)
     vim.cmd("DiffviewFocusFiles")
     local view = require("diffview.lib").get_current_view()
+    if view == nil then
+        vim.notify("Could not find Diffview view", vim.log.levels.ERROR)
+        return
+    end
     local files = view.panel:ordered_file_list()
     local layout = view.cur_layout
-    print(layout, layout.b)
     for _, file in ipairs(files) do
         if file.path == file_name then
             view:set_file(file)
@@ -57,7 +63,6 @@ M.get_location = function()
     elseif layout.b.file.bufnr == bufnr then
         file_name = layout.b.file.path
         current_line_changes = { new_line = vim.api.nvim_win_get_cursor(0)[1], old_line = nil }
-        vim.print(file_name, current_line_changes)
         return file_name, current_line_changes
     end
     return nil, nil, "Line location can only be determined within reviewer window"
