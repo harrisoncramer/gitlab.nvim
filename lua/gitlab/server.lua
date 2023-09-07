57b842cad5745a1edee5af186c0f0e7726fbbc6b
Harrison (Harry) Cramer <32515581+harrisoncramer@users.noreply.github.com>
2023-09-06 20:25:54 -0400

    Feat: Windows Support (#57)
    This MR adds support for Windows machines

--- lua/gitlab/server.lua
+++ lua/gitlab/server.lua
@@ -46,15 +46,21 @@ M.build = function(override)
   local file_path = u.current_file_path()
   local parent_dir = vim.fn.fnamemodify(file_path, ":h:h:h:h")
   state.settings.bin_path = parent_dir
-  state.settings.bin = parent_dir .. "/bin"
+  state.settings.bin = parent_dir .. (u.is_windows() and "\\bin.exe" or "/bin")
 
   if not override then
     local binary_exists = vim.loop.fs_stat(state.settings.bin)
     if binary_exists ~= nil then return end
   end
 
-  local command = string.format("cd %s && make", state.settings.bin_path)
-  local installCode = os.execute(command .. "> /dev/null")
+  local cmd = u.is_windows() and
+      'cd cmd && go build -o bin.exe && move bin.exe ..\\' or
+      'cd cmd && go build -o bin && mv bin ../bin'
+
+  local command = string.format(cmd, state.settings.bin_path)
+  local null = u.is_windows() and " >NUL" or " > /dev/null"
+  print(command .. null)
+  local installCode = os.execute(command .. null)
   if installCode ~= 0 then
     vim.notify("Could not install gitlab.nvim!", vim.log.levels.ERROR)
     return false
