fc02cf18c9832b0984c76a7d02f444ac9b12e396
Harrison Cramer <harrisoncramer@gmail.com>
2023-09-09 13:15:08 -0400

    Adds functionality for detecting stale branches

--- lua/gitlab/reviewer/init.lua
+++ lua/gitlab/reviewer/init.lua
@@ -13,6 +13,7 @@ local reviewer_map = {
   diffview = diffview
 }
 
+
 M.init = function()
   local reviewer = reviewer_map[state.settings.reviewer]
   if reviewer == nil then
@@ -20,8 +21,49 @@ M.init = function()
     return
   end
 
-  M.open = reviewer.open
-  -- Opens the reviewer window
+  -- Opens the reviewer window. If either branch is out of date,
+  -- prompts the user to pull down, then opens the reviewer
+  M.open = function()
+    local branch = vim.fn.system({ "git", "rev-parse", "--abbrev-ref", "HEAD" }):gsub("%s+", "")
+    if branch == "main" or branch == "master" then
+      return -- Must run reviews on feature branches
+    end
+
+    local target_not_ready, source_not_ready, err = M.ready_to_review(state.INFO.source_branch)
+    if err then
+      vim.notify(err, vim.log.levels.ERROR)
+      return
+    end
+
+    if target_not_ready or source_not_ready then
+      local opts = {
+        string.format("Yes, pull \"%s\" and/or \"%s\"", state.INFO.source_branch, state.INFO.target_branch), "No, cancel" }
+      vim.ui.select(opts, {
+        prompt =
+        'Your local repository is out-of-date. Pull branches?',
+      }, function(choice)
+        if not choice or choice == "No, cancel" then return end
+        if target_not_ready then
+          vim.fn.system({ "git", "pull", "--no-edit", "--no-rebase", "--ff-only", "origin", state.INFO.target_branch })
+          if vim.v.shell_error ~= 0 then
+            vim.notify(string.format("Could not pull %s branch", state.INFO.target_branch), vim.log.levels.ERROR)
+            return
+          end
+        end
+        if source_not_ready then
+          vim.fn.system({ "git", "pull", "--no-edit", "--no-rebase", "--ff-only", "origin", state.INFO.source_branch })
+          if vim.v.shell_error ~= 0 then
+            vim.notify(string.format("Could not pull %s branch", state.INFO.source_branch), vim.log.levels.ERROR)
+            return
+          end
+        end
+
+        reviewer.open()
+      end)
+    else
+      reviewer.open()
+    end
+  end
 
   M.jump = reviewer.jump
   -- Jumps to the location provided in the reviewer window
@@ -35,5 +77,32 @@ M.init = function()
   -- file_name, {new_line, old_line}, error
 end
 
+M.ready_to_review = function()
+  vim.fn.system({ "git", "fetch" })
+  if vim.v.shell_error ~= 0 then
+    return nil, nil, "Could not fetch remote changes"
+  end
+
+  local target_output = vim.fn.system({ "git", "rev-list",
+    string.format("%s..origin/%s", state.INFO.target_branch, state.INFO.target_branch) })
+  if vim.v.shell_error ~= 0 then
+    return nil, nil, string.format("Error checking status of %s branch", state.INFO.target_branch)
+  end
+
+  local source_output = vim.fn.system({ "git", "rev-list",
+    string.format("%s..origin/%s", state.INFO.source_branch, state.INFO.source_branch) })
+  if vim.v.shell_error ~= 0 then
+    return nil, nil, string.format("Error checking status of %s branch", state.INFO.source_branch)
+  end
+
+  print(string.format("Your target branch output is: %s", target_output))
+  print(string.format("Your source branch output is: %s", source_output))
+
+  if target_output ~= "" or source_output ~= "" then
+    return target_output ~= "", source_output ~= "", nil
+  end
+
+  return nil, nil, nil
+end
 
 return M
