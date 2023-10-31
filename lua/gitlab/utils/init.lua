6b7e67b3250d49a312fcb1cdaad34dd7052583f1
Harrison (Harry) Cramer <32515581+harrisoncramer@users.noreply.github.com>
2023-10-31 18:46:18 -0400

    Fixes #71 (#75)
    This MR fixes the check for The `diffview` reviewer. Rather than checking for the presence of the command, we check for the module itself.

--- lua/gitlab/utils/init.lua
+++ lua/gitlab/utils/init.lua
@@ -6,18 +6,17 @@ M.get_current_line_number = function()
 end
 
 M.has_reviewer = function(reviewer)
-  local has_reviewer
   if reviewer == "diffview" then
-    has_reviewer = vim.fn.exists(":DiffviewOpen") ~= 0
+    local diffview_ok, _ = pcall(require, "diffview")
+    if not diffview_ok then
+      error("Please install diffview or change your reviewer")
+    end
   else
-    has_reviewer = vim.fn.executable("delta") == 1
+    local has_reviewer = vim.fn.executable("delta") == 1
+    if not has_reviewer then
+      error(string.format("Please install delta or change your reviewer", reviewer))
+    end
   end
-
-  if not has_reviewer then
-    error(string.format("Please install %s or change your reviewer", reviewer))
-  end
-
-  return has_reviewer
 end
 
 M.is_windows = function()
