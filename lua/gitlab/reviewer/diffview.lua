b0cb0b246ce200371d710206347a8d2c56e73167
johnybx <johnybx@users.noreply.github.com>
2023-11-30 23:04:21 +0100

    this change does not belong to this PR

--- lua/gitlab/reviewer/diffview.lua
+++ lua/gitlab/reviewer/diffview.lua
@@ -16,15 +16,9 @@ M.open = function()
     return
   end
 
-  vim.api.nvim_command(string.format("DiffviewOpen %s", diff_refs.base_sha))
+  vim.api.nvim_command(string.format("DiffviewOpen %s..%s", diff_refs.base_sha, diff_refs.head_sha))
   M.tabnr = vim.api.nvim_get_current_tabpage()
 
-  if vim.fn.trim(vim.fn.system("git rev-parse HEAD")) ~= diff_refs.head_sha then
-    u.notify(
-      "Current branch does not have correct HEAD revision, please pull " .. diff_refs.head_sha,
-      vim.log.levels.WARN
-    )
-  end
   if state.INFO.has_conflicts then
     u.notify("This merge request has conflicts!", vim.log.levels.WARN)
   end
