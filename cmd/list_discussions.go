52b9402bff2616695d64e57bccd361d438943d2b
Jakub F. Bortlík <jakub.bortlik@proton.me>
2024-02-14 23:36:28 +0100

    Fix: Iterate over several pages to get all discussions

--- cmd/list_discussions.go
+++ cmd/list_discussions.go
@@ -62,42 +62,49 @@ func (a *api) listDiscussionsHandler(w http.ResponseWriter, r *http.Request) {
 		handleError(w, err, "Could not unmarshal request body", http.StatusBadRequest)
 	}
 
-	mergeRequestDiscussionOptions := gitlab.ListMergeRequestDiscussionsOptions{
-		Page:    1,
-		PerPage: 250,
-	}
-
-	discussions, res, err := a.client.ListMergeRequestDiscussions(a.projectInfo.ProjectId, a.projectInfo.MergeId, &mergeRequestDiscussionOptions, nil)
-
-	if err != nil {
-		handleError(w, err, "Could not list discussions", http.StatusInternalServerError)
-		return
-	}
-
-	if res.StatusCode >= 300 {
-		handleError(w, GenericError{endpoint: "/mr/discussions/list"}, "Could not list discussions", res.StatusCode)
-		return
-	}
-
-	/* Filter out any discussions started by a blacklisted user
-	and system discussions, then return them sorted by created date */
 	var unlinkedDiscussions []*gitlab.Discussion
 	var linkedDiscussions []*gitlab.Discussion
-	for _, discussion := range discussions {
-		if discussion.Notes == nil || len(discussion.Notes) == 0 || Contains(requestBody.Blacklist, discussion.Notes[0].Author.Username) > -1 {
-			continue
+
+	/* Repeat requests for multiple pages, as the maximum number of
+	   items per page is 100 which may not be enough in bigger MRs. */
+	/* TODO: Replace the hardcoded limit by the correct number that can
+	   be obtained from the total number of items (X-Total). */
+	for i := 1; i <= 3; i++ {
+		mergeRequestDiscussionOptions := gitlab.ListMergeRequestDiscussionsOptions{
+			Page:    i,
+			PerPage: 100,
 		}
-		for _, note := range discussion.Notes {
-			if note.Type == gitlab.NoteTypeValue("DiffNote") {
-				linkedDiscussions = append(linkedDiscussions, discussion)
-				break
-			} else if !note.System && note.Position == nil {
-				unlinkedDiscussions = append(unlinkedDiscussions, discussion)
-				break
+
+		discussions, res, err := a.client.ListMergeRequestDiscussions(a.projectInfo.ProjectId, a.projectInfo.MergeId, &mergeRequestDiscussionOptions, nil)
+
+		if err != nil {
+			handleError(w, err, "Could not list discussions", http.StatusInternalServerError)
+			return
+		}
+
+		if res.StatusCode >= 300 {
+			handleError(w, GenericError{endpoint: "/mr/discussions/list"}, "Could not list discussions", res.StatusCode)
+			return
+		}
+
+		/* Filter out any discussions started by a blacklisted user
+		   and system discussions, then return them sorted by created date */
+		for _, discussion := range discussions {
+			if discussion.Notes == nil || len(discussion.Notes) == 0 || Contains(requestBody.Blacklist, discussion.Notes[0].Author.Username) > -1 {
+				continue
+			}
+			for _, note := range discussion.Notes {
+				if note.Type == gitlab.NoteTypeValue("DiffNote") {
+					linkedDiscussions = append(linkedDiscussions, discussion)
+					break
+				} else if !note.System && note.Position == nil {
+					unlinkedDiscussions = append(unlinkedDiscussions, discussion)
+					break
+				}
 			}
 		}
-	}
 
+	}
 	sortedLinkedDiscussions := SortableDiscussions(linkedDiscussions)
 	sortedUnlinkedDiscussions := SortableDiscussions(unlinkedDiscussions)
 
