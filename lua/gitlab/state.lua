9cc1f5280d500c9c85942b8b6279250e64ac5399
Jakub F. Bortlík <jakub.bortlik@proton.me>
2024-02-19 10:58:08 +0100

    Feat: Show Help keymap in discussion tree winbar

--- lua/gitlab/state.lua
+++ lua/gitlab/state.lua
@@ -79,7 +79,8 @@ M.settings = {
         discussions_content = "%#Comment#" .. discussions_content
         notes_content = "%#Text#" .. notes_content
       end
-      return " " .. discussions_content .. " %#Comment#| " .. notes_content
+      local help = "%#Comment#%=Help: " .. t.help_keymap:gsub(" ", "<space>") .. " "
+      return " " .. discussions_content .. " %#Comment#| " .. notes_content .. help
     end,
   },
   merge = {
