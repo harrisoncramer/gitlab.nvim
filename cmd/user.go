56fa9a86f8677d458842a9243e3e8bc9085d957d
Harrison Cramer <harrisoncramer@gmail.com>
2024-02-18 20:38:46 -0500

    Updated cmd/user.go

--- cmd/user.go
+++ cmd/user.go
@@ -1,7 +1,25 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func (a *api) meHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
+	if r.Method != http.MethodGet {
+		w.Header().Set("Access-Control-Allow-Methods", fmt.Sprintf("%s", http.MethodGet))
+		handleError(w, InvalidRequestError{}, "Expected GET", http.StatusMethodNotAllowed)
+		return
+	}
+
+	user, res, err := a.client.CurrentUser()
+
+	if err != nil {
+		handleError(w, err, "Failed to get current user", http.StatusInternalServerError)
+		return
+	}
+
+  if res.StatusCode >= 300 
+
 }
