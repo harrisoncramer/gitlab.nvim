da72db43954d35460c09c7ec8e64022ba29acbc0
Harrison Cramer <harrisoncramer@gmail.com>
2024-02-21 20:00:07 -0500

    Updated lua/gitlab/reviewer/location.lua

--- lua/gitlab/reviewer/location.lua
+++ lua/gitlab/reviewer/location.lua
@@ -9,7 +9,8 @@ local M = {}
 ---@param file_name string
 ---@param old_line number
 ---@param new_line number
-M.build_location_data = function(current_file, modification_type, file_name, old_line, new_line, range)
+---@param visual_range LineRange | nil
+M.build_location_data = function(current_file, modification_type, file_name, old_line, new_line, visual_range)
   local data = hunks.parse_hunks_and_diff(current_file, state.INFO.target_branch)
 
   ---@type ReviewerInfo
@@ -34,44 +35,25 @@ M.build_location_data = function(current_file, modification_type, file_name, old
     payload.new_line = new_line
   end
 
-  if range == nil then
+  if visual_range == nil then
     return payload
   end
 
-  -- If there's a range, use the start of the visual selection, not the current line
-  local current_line = range and range.start_line or vim.api.nvim_win_get_cursor(0)[1]
+  local start_range_info = hunks.get_start_range(modification_type, visual_range, data)
+  local end_range_info = hunks.get_end_range(modification_type, visual_range, data)
 
-  -- If leaving a multi-line comment, we want to also add range_info to the payload.
-  local is_new = payload.new_line ~= nil
-  local current_line_info = is_new and hunks.get_lines_from_hunks(data.hunks, payload.new_line, is_new)
-    or hunks.get_lines_from_hunks(data.hunks, payload.old_line, is_new)
-  local type = is_new and "new" or "old"
-
-  ---@type ReviewerRangeInfo
-  local range_info = { start = {}, ["end"] = {} }
-
-  if current_line == range.start_line then
-    range_info.start.old_line = current_line_info.old_line
-    range_info.start.new_line = current_line_info.new_line
-    range_info.start.type = type
-  else
-    local start_line_info = hunks.get_lines_from_hunks(data.hunks, range.start_line, is_new)
-    range_info.start.old_line = start_line_info.old_line
-    range_info.start.new_line = start_line_info.new_line
-    range_info.start.type = type
-  end
-  if current_line == range.end_line then
-    range_info["end"].old_line = current_line_info.old_line
-    range_info["end"].new_line = current_line_info.new_line
-    range_info["end"].type = type
-  else
-    local end_line_info = hunks.get_lines_from_hunks(data.hunks, range.end_line, is_new)
-    range_info["end"].old_line = end_line_info.old_line
-    range_info["end"].new_line = end_line_info.new_line
-    range_info["end"].type = type
-  end
-
-  payload.range_info = range_info
+  payload.range_info = {
+    start = {
+      old_line = start_range_info.old_line,
+      new_line = start_range_info.new_line,
+      type = start_range_info.type
+    },
+    ["end"] = {
+      old_line = end_range_info.old_line,
+      new_line = end_range_info.new_line,
+      type = end_range_info.type
+    }
+  }
 
   return payload
 end
