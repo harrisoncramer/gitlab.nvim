8b533730a2db8fc8ff31270080c99e2a664d60e8
Harrison Cramer <harrisoncramer@gmail.com>
2024-02-21 22:15:14 -0500

    Updated lua/gitlab/reviewer/location.lua

--- lua/gitlab/reviewer/location.lua
+++ lua/gitlab/reviewer/location.lua
@@ -4,15 +4,12 @@ local M = {}
 
 ---Takes in information about the current changes, such as the file name, modification type of the diff, and the line numbers
 ---and builds the appropriate payload when creating a comment.
----@param current_file string
 ---@param modification_type string
 ---@param file_name string
 ---@param old_line number
 ---@param new_line number
 ---@param visual_range LineRange | nil
-M.build_location_data = function(current_file, modification_type, file_name, old_line, new_line, visual_range)
-  local data = hunks.parse_hunks_and_diff(current_file, state.INFO.target_branch)
-
+M.build_location_data = function(modification_type, file_name, old_line, new_line, visual_range)
   ---@type ReviewerInfo
   local payload = {
     file_name = file_name,
@@ -25,11 +22,11 @@ M.build_location_data = function(current_file, modification_type, file_name, old
   if modification_type == "added" then
     payload.old_line = nil
     payload.new_line = new_line
-    -- Comment on deleted line: Include only new_line in payload.
+    -- Comment on deleted line: Include only old_line in payload.
   elseif modification_type == "deleted" then
     payload.old_line = old_line
     payload.new_line = nil
-    -- The line was not found in any hunks, only send the old line number
+    -- The line was not found in any hunks, send both lines.
   elseif modification_type == "unmodified" or modification_type == "bad_file_unmodified" then
     payload.old_line = old_line
     payload.new_line = new_line
@@ -39,8 +36,9 @@ M.build_location_data = function(current_file, modification_type, file_name, old
     return payload
   end
 
-  local start_range_info = hunks.get_start_range(modification_type, visual_range, data)
-  local end_range_info = hunks.get_end_range(modification_type, visual_range, data)
+  -- Assume at top of visual range in the new buffer...
+  local start_range_info = hunks.get_start_range(new_line, old_line, modification_type)
+  local end_range_info = hunks.get_end_range(new_line, old_line, visual_range)
 
   payload.range_info = {
     start = {
