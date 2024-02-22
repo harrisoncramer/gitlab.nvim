540ac4532d5959db9d1d29a333ba9253f70b310a
Harrison Cramer <harrisoncramer@gmail.com>
2024-02-22 07:09:15 -0500

    Updated lua/gitlab/reviewer/location.lua

--- lua/gitlab/reviewer/location.lua
+++ lua/gitlab/reviewer/location.lua
@@ -37,9 +37,11 @@ M.build_location_data = function(current_file, modification_type, file_name, old
     return payload
   end
 
+  -- TODO: Assume visual range start is at top of block
+
   -- Assume at top of visual range in the new buffer...
-  local start_range_info = hunks.get_start_range(new_line, old_line, modification_type)
-  local end_range_info = hunks.get_end_range(new_line, old_line, visual_range, current_file)
+  local start_range_info = M.get_start_range(visual_range, modification_type)
+  local end_range_info = M.get_end_range(new_line, old_line, visual_range, current_file)
 
   payload.range_info = {
     start = {
@@ -55,6 +57,55 @@ M.build_location_data = function(current_file, modification_type, file_name, old
   }
 
   return payload
+end
+
+-- Returns the matching line from the other SHA (either old or new) based on
+-- the line provided. For instance, line 12 in the new SHA may be scroll-linked
+-- to line 10 in the old SHA.
+---@param line number
+---@param old_sha boolean
+---@return number
+M.get_matching_linenr = function(line, old_sha)
+  local is_current_sha = require("gitlab.reviewer").is_current_sha()
+  if (is_current_sha and not old_sha) or (not is_current_sha and old_sha) then
+    return line
+  end
+  return 0 -- Get the line from the other SHA
+end
+
+-- Given a new_line and old_line from the start of a ranged comment, returns the start
+-- range information for the Gitlab payload
+---@param visual_range LineRange
+---@param modification_type string
+---@return ReviewerLineInfo
+M.get_start_range = function(visual_range, modification_type)
+  return {
+    new_line = M.get_matching_linenr(visual_range.start_line, false),
+    old_line = M.get_matching_linenr(visual_range.start_line, true),
+    type = modification_type == "added" and "new" or "old"
+  }
+end
+
+-- Given a modification type, a range, and the hunk data, returns the end range information
+-- for the Gitlab payload
+---@param new_line number
+---@param old_line number
+---@param visual_range LineRange
+---@return ReviewerLineInfo
+M.get_end_range = function(new_line, old_line, visual_range, current_file)
+  -- TODO:
+  -- Get the  the visual range to detect the end of the range and new lines.✓
+  -- from the current SHA and the old SHA.
+  -- Once we have those lines, we pass them into the modification_type function, to get the type.
+  -- Pass all three to the result table.
+  -- local is_current = reviewer.is_current_sha()
+  -- local lines_spanned = visual_range.end_line - visual_range.start_line
+  -- local modification_type = M.get_modification_type()
+  return {
+    old_line = 3,
+    new_line = 3,
+    type = "old",
+  }
 end
 
 return M
