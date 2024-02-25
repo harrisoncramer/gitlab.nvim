727bbe193816ce39570c53a09569776f92e2f70d
Harrison Cramer <harrisoncramer@gmail.com>
2024-02-25 10:05:42 -0500

    Updated lua/gitlab/hunks/init.lua

--- lua/gitlab/hunks/init.lua
+++ lua/gitlab/hunks/init.lua
@@ -273,6 +273,7 @@ M.calculate_matching_line_new = function(old_sha, new_sha, file_path, line_numbe
     end
   end
 
+  -- TODO: Possibly handle lines that are out of range in the new files
   return line_number
 end
 
