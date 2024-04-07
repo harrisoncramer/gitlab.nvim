f748799f71652a2e7546316adb4508ce8465d1e7
Harrison Cramer <harrisoncramer@gmail.com>
2024-04-07 15:19:53 -0400

    formatting

--- lua/gitlab/state.lua
+++ lua/gitlab/state.lua
@@ -70,10 +70,10 @@ M.settings = {
     winbar = function(t)
       local discussions_content = t.resolvable_discussions ~= 0
           and string.format("Discussions (%d/%d)", t.resolved_discussions, t.resolvable_discussions)
-          or "Discussions"
+        or "Discussions"
       local notes_content = t.resolvable_notes ~= 0
           and string.format("Notes (%d/%d)", t.resolved_notes, t.resolvable_notes)
-          or "Notes"
+        or "Notes"
       if t.name == "Discussions" then
         notes_content = "%#Comment#" .. notes_content
         discussions_content = "%#Text#" .. discussions_content
