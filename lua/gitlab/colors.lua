c9ed8067fd8e04635d0b4b417f0e595c9f5fe6d6
Harrison (Harry) Cramer <32515581+harrisoncramer@users.noreply.github.com>
2024-08-08 11:29:52 -0400

    Update colors.lua

--- lua/gitlab/colors.lua
+++ lua/gitlab/colors.lua
@@ -15,7 +15,7 @@ local discussion = colors.discussion_tree
 vim.api.nvim_set_hl(0, "GitlabUsername", u.get_colors_for_group(discussion.username))
 vim.api.nvim_set_hl(0, "GitlabMention", u.get_colors_for_group(discussion.mention))
 vim.api.nvim_set_hl(0, "GitlabDate", u.get_colors_for_group(discussion.date))
-vim.api.nvim_set_hl(0, "GitlabChevron", u.get_colors_for_group(discussion.chevron))
+vim.api.nvim_set_hl(0, "GitlabExpander", u.get_colors_for_group(discussion.expander))
 vim.api.nvim_set_hl(0, "GitlabDirectory", u.get_colors_for_group(discussion.directory))
 vim.api.nvim_set_hl(0, "GitlabDirectoryIcon", u.get_colors_for_group(discussion.directory_icon))
 vim.api.nvim_set_hl(0, "GitlabFileName", u.get_colors_for_group(discussion.file_name))
