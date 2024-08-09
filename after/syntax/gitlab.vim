harrisoncramer/gitlab.nvim

commit 076647ace5fa5944e81a9f026f3572bc770be77f

Feat: Customize discussion tree chevrons (#339)
feat: Adds the ability to customize the chevron icons in the discussion tree

after/syntax/gitlab.vim
@@ -2,22 +2,25 @@ if filereadable($VIMRUNTIME . '/syntax/markdown.vim')
   source $VIMRUNTIME/syntax/markdown.vim
 endif
 
-syntax match Username "\%([]\)\@<= @\S*"
-syntax match Mention "\%([] \)\@<!@\S*"
 syntax match Date "\v\d+\s+\w+\s+ago"
-syntax match ChevronDown ""
-syntax match ChevronRight ""
-syntax match Resolved /\s✓\s\?/
-syntax match Unresolved /\s-\s\?/
-syntax match Pencil //
-
-highlight link Username GitlabUsername
-highlight link Mention GitlabMention
 highlight link Date GitlabDate
-highlight link ChevronDown GitlabChevron
-highlight link ChevronRight GitlabChevron
-highlight link Resolved GitlabResolved
+
+execute 'syntax match Unresolved /\s' . g:gitlab_discussion_tree_unresolved . '\s\?/'
 highlight link Unresolved GitlabUnresolved
-highlight link Pencil GitlabDraft
+
+execute 'syntax match Resolved /\s' . g:gitlab_discussion_tree_resolved . '\s\?/'
+highlight link Resolved GitlabResolved
+
+execute 'syntax match GitlabDiscussionOpen /^' . g:gitlab_discussion_tree_expander_open . '/'
+highlight link GitlabDiscussionOpen GitlabExpander
+
+execute 'syntax match GitlabDiscussionClosed /^' . g:gitlab_discussion_tree_expander_closed . '/'
+highlight link GitlabDiscussionClosed GitlabExpander
+
+execute 'syntax match Draft /' . g:gitlab_discussion_tree_draft . '/'
+highlight link Draft GitlabDraft
+
+execute 'syntax match Username "@\w\+"'
+highlight link Username GitlabUsername
 
 let b:current_syntax = "gitlab"

lua/gitlab/actions/discussions/tree.lua
@@ -325,16 +325,17 @@ M.nui_tree_prepare_node = function(node)
 
   for i, text in ipairs(texts) do
     local line = NuiLine()
+    local expanders = state.settings.discussion_tree.expanders
 
-    line:append(string.rep("  ", node._depth - 1))
+    line:append(string.rep(expanders.indentation, node._depth - 1))
 
     if i == 1 and node:has_children() then
-      line:append(node:is_expanded() and " " or " ")
+      line:append(node:is_expanded() and expanders.expanded or expanders.collapsed)
       if node.icon then
         line:append(node.icon .. " ", node.icon_hl)
       end
     else
-      line:append("  ")
+      line:append(expanders.indentation)
     end
 
     line:append(text, node.text_hl)

lua/gitlab/colors.lua
@@ -2,12 +2,20 @@ local state = require("gitlab.state")
 local u = require("gitlab.utils")
 
 local colors = state.settings.colors
-local discussion = colors.discussion_tree
 
+-- Set icons into global vim variables for syntax matching
+local expanders = state.settings.discussion_tree.expanders
+vim.g.gitlab_discussion_tree_expander_open = expanders.expanded
+vim.g.gitlab_discussion_tree_expander_closed = expanders.collapsed
+vim.g.gitlab_discussion_tree_draft = ""
+vim.g.gitlab_discussion_tree_resolved = "✓"
+vim.g.gitlab_discussion_tree_unresolved = "-"
+
+local discussion = colors.discussion_tree
 vim.api.nvim_set_hl(0, "GitlabUsername", u.get_colors_for_group(discussion.username))
 vim.api.nvim_set_hl(0, "GitlabMention", u.get_colors_for_group(discussion.mention))
 vim.api.nvim_set_hl(0, "GitlabDate", u.get_colors_for_group(discussion.date))
-vim.api.nvim_set_hl(0, "GitlabChevron", u.get_colors_for_group(discussion.chevron))
+vim.api.nvim_set_hl(0, "GitlabExpander", u.get_colors_for_group(discussion.expander))
 vim.api.nvim_set_hl(0, "GitlabDirectory", u.get_colors_for_group(discussion.directory))
 vim.api.nvim_set_hl(0, "GitlabDirectoryIcon", u.get_colors_for_group(discussion.directory_icon))
 vim.api.nvim_set_hl(0, "GitlabFileName", u.get_colors_for_group(discussion.file_name))

lua/gitlab/state.lua
@@ -139,6 +139,11 @@ M.settings = {
     temp_registers = {},
   },
   discussion_tree = {
+    expanders = {
+      expanded = " ",
+      collapsed = " ",
+      indentation = "  ",
+    },
     auto_open = true,
     default_view = "discussions",
     blacklist = {},
@@ -219,7 +224,7 @@ M.settings = {
       username = "Keyword",
       mention = "WarningMsg",
       date = "Comment",
-      chevron = "DiffviewNonText",
+      expander = "DiffviewNonText",
       directory = "Directory",
       directory_icon = "DiffviewFolderSign",
       file_name = "Normal",

commit 221a3fae5ff6bb2eb71187771bad2b089dd97c0c

Fix: Discussion tree highlighting (#342)
fix: Fixes highlighting regressions from #339

after/syntax/gitlab.vim
@@ -11,16 +11,20 @@ highlight link Unresolved GitlabUnresolved
 execute 'syntax match Resolved /\s' . g:gitlab_discussion_tree_resolved . '\s\?/'
 highlight link Resolved GitlabResolved
 
-execute 'syntax match GitlabDiscussionOpen /^' . g:gitlab_discussion_tree_expander_open . '/'
+execute 'syntax match GitlabDiscussionOpen /^\s*' . g:gitlab_discussion_tree_expander_open . '/'
 highlight link GitlabDiscussionOpen GitlabExpander
 
-execute 'syntax match GitlabDiscussionClosed /^' . g:gitlab_discussion_tree_expander_closed . '/'
+execute 'syntax match GitlabDiscussionClosed /^\s*' . g:gitlab_discussion_tree_expander_closed . '/'
 highlight link GitlabDiscussionClosed GitlabExpander
 
 execute 'syntax match Draft /' . g:gitlab_discussion_tree_draft . '/'
 highlight link Draft GitlabDraft
 
-execute 'syntax match Username "@\w\+"'
+execute 'syntax match Username "@[a-zA-Z0-9.]\+"'
 highlight link Username GitlabUsername
 
-let b:current_syntax = "gitlab"
+execute 'syntax match Mention "\%(' . g:gitlab_discussion_tree_expander_open . '\|'
+  \ . g:gitlab_discussion_tree_expander_closed . '\)\@<!@[a-zA-Z0-9.]*"'
+highlight link Mention GitlabMention
+
+let b:current_syntax = 'gitlab'