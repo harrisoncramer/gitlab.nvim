9cded9033c2523f15e823d39861be7ec35545e8e
Richard Domingo <chardskarth@gmail.com>
2024-08-15 15:12:47 +0800

    chore: fix failing tests

--- cmd/merge_requests_test.go
+++ cmd/merge_requests_test.go
@@ -22,21 +22,21 @@ func listProjectMergeRequestsErr(pid interface{}, opt *gitlab.ListProjectMergeRe
 
 func TestMergeRequestHandler(t *testing.T) {
 	t.Run("Should fetch merge requests", func(t *testing.T) {
-		request := makeRequest(t, http.MethodGet, "/merge_requests", nil)
+		request := makeRequest(t, http.MethodPost, "/merge_requests", ListMergeRequestRequest{})
 		server, _ := createRouterAndApi(fakeClient{listProjectMergeRequests: listProjectMergeRequests200})
 		data := serveRequest(t, server, request, ListMergeRequestResponse{})
 		assert(t, data.Message, "Merge requests fetched successfully")
 		assert(t, data.Status, http.StatusOK)
 	})
 	t.Run("Should handle an error", func(t *testing.T) {
-		request := makeRequest(t, http.MethodGet, "/merge_requests", nil)
+		request := makeRequest(t, http.MethodPost, "/merge_requests", ListMergeRequestRequest{})
 		server, _ := createRouterAndApi(fakeClient{listProjectMergeRequests: listProjectMergeRequestsErr})
 		data := serveRequest(t, server, request, ErrorResponse{})
 		assert(t, data.Message, "Failed to list merge requests")
 		assert(t, data.Status, http.StatusInternalServerError)
 	})
 	t.Run("Should handle not having any merge requests with 404", func(t *testing.T) {
-		request := makeRequest(t, http.MethodGet, "/merge_requests", nil)
+		request := makeRequest(t, http.MethodPost, "/merge_requests", ListMergeRequestRequest{})
 		server, _ := createRouterAndApi(fakeClient{listProjectMergeRequests: listProjectMergeRequestsEmpty})
 		data := serveRequest(t, server, request, ErrorResponse{})
 		assert(t, data.Message, "No merge requests found")
