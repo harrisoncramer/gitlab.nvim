f63060789a4400e9b7e60737e914b4103b6cc075
Harrison Cramer <harrisoncramer@gmail.com>
2024-08-17 17:03:57 -0400

    Fixed merge_test.go

--- cmd/merge_test.go
+++ cmd/merge_test.go
@@ -6,47 +6,68 @@ import (
 	"testing"
 
 	"github.com/xanzy/go-gitlab"
+	mock_main "gitlab.com/harrisoncramer/gitlab.nvim/cmd/mocks"
 )
 
-func acceptMergeRequest(pid interface{}, mergeRequest int, opt *gitlab.AcceptMergeRequestOptions, options ...gitlab.RequestOptionFunc) (*gitlab.MergeRequest, *gitlab.Response, error) {
-	return &gitlab.MergeRequest{}, makeResponse(http.StatusOK), nil
+var testAcceptMergeRequestPayload = AcceptMergeRequestRequest{
+	Squash:        false,
+	SquashMessage: "Squash me!",
+	DeleteBranch:  false,
 }
 
-func acceptMergeRequestErr(pid interface{}, mergeRequest int, opt *gitlab.AcceptMergeRequestOptions, options ...gitlab.RequestOptionFunc) (*gitlab.MergeRequest, *gitlab.Response, error) {
-	return nil, nil, errors.New("Some error from Gitlab")
-}
-
-func acceptAndMergeNon200(pid interface{}, mergeRequest int, opt *gitlab.AcceptMergeRequestOptions, options ...gitlab.RequestOptionFunc) (*gitlab.MergeRequest, *gitlab.Response, error) {
-	return nil, makeResponse(http.StatusSeeOther), nil
+var testAcceptMergeRequestOpts = gitlab.AcceptMergeRequestOptions{
+	Squash:                   &testAcceptMergeRequestPayload.Squash,
+	ShouldRemoveSourceBranch: &testAcceptMergeRequestPayload.DeleteBranch,
+	SquashCommitMessage:      &testAcceptMergeRequestPayload.SquashMessage,
 }
 
 func TestAcceptAndMergeHandler(t *testing.T) {
 	t.Run("Accepts and merges a merge request", func(t *testing.T) {
-		request := makeRequest(t, http.MethodPost, "/mr/merge", AcceptMergeRequestRequest{})
-		server, _ := CreateRouterAndApi(fakeClient{acceptMergeRequest: acceptMergeRequest})
+		client := mock_main.NewMockClient(t)
+		mock_main.WithMr(t, client, mock_main.MergeId)
+		client.EXPECT().AcceptMergeRequest("", mock_main.MergeId, &testAcceptMergeRequestOpts).Return(&gitlab.MergeRequest{}, makeResponse(http.StatusOK), nil)
+
+		request := makeRequest(t, http.MethodPost, "/mr/merge", testAcceptMergeRequestPayload)
+		server, _ := CreateRouterAndApi(client)
 		data := serveRequest(t, server, request, SuccessResponse{})
+
 		assert(t, data.Message, "MR merged successfully")
 		assert(t, data.Status, http.StatusOK)
 	})
 
 	t.Run("Disallows non-POST methods", func(t *testing.T) {
-		request := makeRequest(t, http.MethodGet, "/mr/merge", AcceptMergeRequestRequest{})
-		server, _ := CreateRouterAndApi(fakeClient{acceptMergeRequest: acceptMergeRequest})
+		client := mock_main.NewMockClient(t)
+		mock_main.WithMr(t, client, mock_main.MergeId)
+		client.EXPECT().AcceptMergeRequest("", mock_main.MergeId, &testAcceptMergeRequestOpts).Return(&gitlab.MergeRequest{}, makeResponse(http.StatusOK), nil)
+
+		request := makeRequest(t, http.MethodPatch, "/mr/merge", testAcceptMergeRequestPayload)
+		server, _ := CreateRouterAndApi(client)
 		data := serveRequest(t, server, request, ErrorResponse{})
+
 		checkBadMethod(t, *data, http.MethodPost)
 	})
 
 	t.Run("Handles errors from Gitlab client", func(t *testing.T) {
-		request := makeRequest(t, http.MethodPost, "/mr/merge", AcceptMergeRequestRequest{})
-		server, _ := CreateRouterAndApi(fakeClient{acceptMergeRequest: acceptMergeRequestErr})
+		client := mock_main.NewMockClient(t)
+		mock_main.WithMr(t, client, mock_main.MergeId)
+		client.EXPECT().AcceptMergeRequest("", mock_main.MergeId, &testAcceptMergeRequestOpts).Return(nil, nil, errors.New("Some error from Gitlab"))
+
+		request := makeRequest(t, http.MethodPost, "/mr/merge", testAcceptMergeRequestPayload)
+		server, _ := CreateRouterAndApi(client)
 		data := serveRequest(t, server, request, ErrorResponse{})
+
 		checkErrorFromGitlab(t, *data, "Could not merge MR")
 	})
 
 	t.Run("Handles non-200s from Gitlab client", func(t *testing.T) {
-		request := makeRequest(t, http.MethodPost, "/mr/merge", AcceptMergeRequestRequest{})
-		server, _ := CreateRouterAndApi(fakeClient{acceptMergeRequest: acceptAndMergeNon200})
+		client := mock_main.NewMockClient(t)
+		mock_main.WithMr(t, client, mock_main.MergeId)
+		client.EXPECT().AcceptMergeRequest("", mock_main.MergeId, &testAcceptMergeRequestOpts).Return(nil, makeResponse(http.StatusSeeOther), nil)
+
+		request := makeRequest(t, http.MethodPost, "/mr/merge", testAcceptMergeRequestPayload)
+		server, _ := CreateRouterAndApi(client)
 		data := serveRequest(t, server, request, ErrorResponse{})
+
 		checkNon200(t, *data, "Could not merge MR", "/mr/merge")
 	})
 }
