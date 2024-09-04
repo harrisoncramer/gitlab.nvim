f6ee6f3a4fb06e825b716966c205956d6a33f8b5
Harrison Cramer <harrisoncramer@gmail.com>
2024-09-04 14:44:46 -0400

    Fixed comment tests

--- cmd/app/comment_test.go
+++ cmd/app/comment_test.go
@@ -36,8 +36,6 @@ func (f fakeCommentClient) DeleteMergeRequestDiscussionNote(pid interface{}, mer
 	return resp, err
 }
 
-// var testCommentDeletionData = DeleteCommentRequest{NoteId: 3, DiscussionId: "abc123"}
-// var testEditCommentData = EditCommentRequest{Comment: "Some comment", NoteId: 3, DiscussionId: "abc123"}
 func TestPostComment(t *testing.T) {
 	var testCommentCreationData = PostCommentRequest{Comment: "Some comment"}
 	t.Run("Creates a new note (unlinked comment)", func(t *testing.T) {
@@ -62,115 +60,65 @@ func TestPostComment(t *testing.T) {
 		assert(t, data.Status, http.StatusOK)
 	})
 
-	//		t.Run("Handles errors from Gitlab client", func(t *testing.T) {
-	//			client := mock_main.NewMockClient(t)
-	//			mock_main.WithMr(t, client)
-	//			client.EXPECT().CreateMergeRequestDiscussion(
-	//				"",
-	//				mock_main.MergeId,
-	//				gomock.Any(),
-	//			).Return(nil, nil, errors.New("Some error from Gitlab"))
-	//
-	//			request := makeRequest(t, http.MethodPost, "/mr/comment", testCommentCreationData)
-	//			server := CreateRouter(client)
-	//			data := serveRequest(t, server, request, ErrorResponse{})
-	//
-	//			checkErrorFromGitlab(t, *data, "Could not create discussion")
-	//		})
-	//
-	//		t.Run("Handles non-200s from Gitlab", func(t *testing.T) {
-	//			client := mock_main.NewMockClient(t)
-	//			mock_main.WithMr(t, client)
-	//			client.EXPECT().CreateMergeRequestDiscussion(
-	//				"",
-	//				mock_main.MergeId,
-	//				gomock.Any(),
-	//			).Return(nil, makeResponse(http.StatusSeeOther), nil)
-	//
-	//			request := makeRequest(t, http.MethodPost, "/mr/comment", testCommentCreationData)
-	//			server := CreateRouter(client)
-	//			data := serveRequest(t, server, request, ErrorResponse{})
-	//
-	//			checkNon200(t, *data, "Could not create discussion", "/mr/comment")
-	//		})
-	//	}
-	//
-	//	func TestDeleteComment(t *testing.T) {
-	//		t.Run("Deletes a comment", func(t *testing.T) {
-	//			client := mock_main.NewMockClient(t)
-	//			mock_main.WithMr(t, client)
-	//			client.EXPECT().DeleteMergeRequestDiscussionNote("", mock_main.MergeId, testCommentDeletionData.DiscussionId, testCommentDeletionData.NoteId).Return(makeResponse(http.StatusOK), nil)
-	//
-	//			request := makeRequest(t, http.MethodDelete, "/mr/comment", testCommentDeletionData)
-	//			server := CreateRouter(client)
-	//			data := serveRequest(t, server, request, CommentResponse{})
-	//
-	//			assert(t, data.SuccessResponse.Message, "Comment deleted successfully")
-	//			assert(t, data.SuccessResponse.Status, http.StatusOK)
-	//		})
-	//
-	//		t.Run("Handles errors from Gitlab client", func(t *testing.T) {
-	//			client := mock_main.NewMockClient(t)
-	//			mock_main.WithMr(t, client)
-	//			client.EXPECT().DeleteMergeRequestDiscussionNote("", mock_main.MergeId, testCommentDeletionData.DiscussionId, testCommentDeletionData.NoteId).Return(nil, errorFromGitlab)
-	//
-	//			request := makeRequest(t, http.MethodDelete, "/mr/comment", testCommentDeletionData)
-	//			server := CreateRouter(client)
-	//			data := serveRequest(t, server, request, ErrorResponse{})
-	//
-	//			checkErrorFromGitlab(t, *data, "Could not delete comment")
-	//		})
-	//
-	//		t.Run("Handles non-200s from Gitlab", func(t *testing.T) {
-	//			client := mock_main.NewMockClient(t)
-	//			mock_main.WithMr(t, client)
-	//			client.EXPECT().DeleteMergeRequestDiscussionNote("", mock_main.MergeId, testCommentDeletionData.DiscussionId, testCommentDeletionData.NoteId).Return(makeResponse(http.StatusSeeOther), nil)
-	//
-	//			request := makeRequest(t, http.MethodDelete, "/mr/comment", testCommentDeletionData)
-	//			server := CreateRouter(client)
-	//			data := serveRequest(t, server, request, ErrorResponse{})
-	//
-	//			checkNon200(t, *data, "Could not delete comment", "/mr/comment")
-	//		})
-	//	}
-	//
-	//	func TestEditComment(t *testing.T) {
-	//		t.Run("Edits a comment", func(t *testing.T) {
-	//			client := mock_main.NewMockClient(t)
-	//			mock_main.WithMr(t, client)
-	//			opts := gitlab.UpdateMergeRequestDiscussionNoteOptions{
-	//				Body: gitlab.Ptr(testEditCommentData.Comment),
-	//			}
-	//			client.EXPECT().UpdateMergeRequestDiscussionNote("", mock_main.MergeId, testEditCommentData.DiscussionId, testEditCommentData.NoteId, &opts).Return(&gitlab.Note{}, makeResponse(http.StatusOK), nil)
-	//
-	//			request := makeRequest(t, http.MethodPatch, "/mr/comment", testEditCommentData)
-	//			server := CreateRouter(client)
-	//			data := serveRequest(t, server, request, CommentResponse{})
-	//
-	//			assert(t, data.SuccessResponse.Message, "Comment updated successfully")
-	//			assert(t, data.SuccessResponse.Status, http.StatusOK)
-	//		})
-	//
-	//		t.Run("Handles errors from Gitlab client", func(t *testing.T) {
-	//			client := mock_main.NewMockClient(t)
-	//			mock_main.WithMr(t, client)
-	//			client.EXPECT().UpdateMergeRequestDiscussionNote("", mock_main.MergeId, testEditCommentData.DiscussionId, testEditCommentData.NoteId, gomock.Any()).Return(nil, nil, errorFromGitlab)
-	//
-	//			request := makeRequest(t, http.MethodPatch, "/mr/comment", testEditCommentData)
-	//			server := CreateRouter(client)
-	//			data := serveRequest(t, server, request, ErrorResponse{})
-	//
-	//			checkErrorFromGitlab(t, *data, "Could not update comment")
-	//		})
-	//
-	//		t.Run("Handles non-200s from Gitlab", func(t *testing.T) {
-	//			client := mock_main.NewMockClient(t)
-	//			mock_main.WithMr(t, client)
-	//			client.EXPECT().UpdateMergeRequestDiscussionNote("", mock_main.MergeId, testEditCommentData.DiscussionId, testEditCommentData.NoteId, gomock.Any()).Return(nil, makeResponse(http.StatusSeeOther), nil)
-	//
-	//			request := makeRequest(t, http.MethodPatch, "/mr/comment", testEditCommentData)
-	//			server := CreateRouter(client)
-	//			data := serveRequest(t, server, request, ErrorResponse{})
-	//			checkNon200(t, *data, "Could not update comment", "/mr/comment")
-	//		})
+	t.Run("Handles errors from Gitlab client", func(t *testing.T) {
+		request := makeRequest(t, http.MethodPost, "/mr/comment", testCommentCreationData)
+		svc := commentService{emptyProjectData, fakeCommentClient{testBase{errFromGitlab: true}}}
+		data := getFailData(t, svc, request)
+		checkErrorFromGitlab(t, data, "Could not create discussion")
+	})
+
+	t.Run("Handles non-200s from Gitlab client", func(t *testing.T) {
+		request := makeRequest(t, http.MethodPost, "/mr/comment", testCommentCreationData)
+		svc := commentService{emptyProjectData, fakeCommentClient{testBase{status: http.StatusSeeOther}}}
+		data := getFailData(t, svc, request)
+		checkNon200(t, data, "Could not create discussion", "/mr/comment")
+	})
+}
+
+func TestDeleteComment(t *testing.T) {
+	var testCommentDeletionData = DeleteCommentRequest{NoteId: 3, DiscussionId: "abc123"}
+	t.Run("Deletes a comment", func(t *testing.T) {
+		request := makeRequest(t, http.MethodDelete, "/mr/comment", testCommentDeletionData)
+		svc := commentService{emptyProjectData, fakeCommentClient{}}
+		data := getSuccessData(t, svc, request)
+		assert(t, data.Message, "Comment deleted successfully")
+		assert(t, data.Status, http.StatusOK)
+	})
+	t.Run("Handles errors from Gitlab client", func(t *testing.T) {
+		request := makeRequest(t, http.MethodDelete, "/mr/comment", testCommentDeletionData)
+		svc := commentService{emptyProjectData, fakeCommentClient{testBase{errFromGitlab: true}}}
+		data := getFailData(t, svc, request)
+		checkErrorFromGitlab(t, data, "Could not delete comment")
+	})
+
+	t.Run("Handles non-200s from Gitlab client", func(t *testing.T) {
+		request := makeRequest(t, http.MethodDelete, "/mr/comment", testCommentDeletionData)
+		svc := commentService{emptyProjectData, fakeCommentClient{testBase{status: http.StatusSeeOther}}}
+		data := getFailData(t, svc, request)
+		checkNon200(t, data, "Could not delete comment", "/mr/comment")
+	})
+}
+
+func TestEditComment(t *testing.T) {
+	var testEditCommentData = EditCommentRequest{Comment: "Some comment", NoteId: 3, DiscussionId: "abc123"}
+	t.Run("Edits a comment", func(t *testing.T) {
+		request := makeRequest(t, http.MethodPatch, "/mr/comment", testEditCommentData)
+		svc := commentService{emptyProjectData, fakeCommentClient{}}
+		data := getSuccessData(t, svc, request)
+		assert(t, data.Message, "Comment updated successfully")
+		assert(t, data.Status, http.StatusOK)
+	})
+	t.Run("Handles errors from Gitlab client", func(t *testing.T) {
+		request := makeRequest(t, http.MethodPatch, "/mr/comment", testEditCommentData)
+		svc := commentService{emptyProjectData, fakeCommentClient{testBase{errFromGitlab: true}}}
+		data := getFailData(t, svc, request)
+		checkErrorFromGitlab(t, data, "Could not update comment")
+	})
+
+	t.Run("Handles non-200s from Gitlab client", func(t *testing.T) {
+		request := makeRequest(t, http.MethodPatch, "/mr/comment", testEditCommentData)
+		svc := commentService{emptyProjectData, fakeCommentClient{testBase{status: http.StatusSeeOther}}}
+		data := getFailData(t, svc, request)
+		checkNon200(t, data, "Could not update comment", "/mr/comment")
+	})
 }
