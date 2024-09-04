68c4eaae2c2482fa5765da6bda4fa76e404a8622
Harrison Cramer <harrisoncramer@gmail.com>
2024-09-04 16:25:25 -0400

    Updated cmd/app/draft_notes_test.go

--- cmd/app/draft_notes_test.go
+++ cmd/app/draft_notes_test.go
@@ -141,130 +141,13 @@ func TestEditDraftNote(t *testing.T) {
 		assert(t, data.Message, "Could not parse draft note ID")
 		assert(t, data.Status, http.StatusBadRequest)
 	})
+	t.Run("Handles empty note", func(t *testing.T) {
+		requestData := testUpdateDraftNoteRequest
+		requestData.Note = ""
+		request := makeRequest(t, http.MethodPatch, "/mr/draft_notes/3", requestData)
+		svc := draftNoteService{emptyProjectData, fakeDraftNoteManager{testBase{status: http.StatusSeeOther}}}
+		data := getFailData(t, svc, request)
+		assert(t, data.Message, "Must provide draft note text")
+		assert(t, data.Status, http.StatusBadRequest)
+	})
 }
-
-// var testDraftNotePublishRequest = DraftNotePublishRequest{
-// 	Note:       3,
-// 	PublishAll: false,
-// }
-
-// func TestEditDraftNote(t *testing.T) {
-// 	t.Run("Edits draft note", func(t *testing.T) {
-// 		client := mock_main.NewMockClient(t)
-// 		mock_main.WithMr(t, client)
-// 		urlId := 10
-// 		client.EXPECT().UpdateDraftNote("", mock_main.MergeId, urlId, gomock.Any()).Return(&gitlab.DraftNote{}, makeResponse(http.StatusOK), nil)
-//
-// 		request := makeRequest(t, http.MethodPatch, fmt.Sprintf("/mr/draft_notes/%d", urlId), testUpdateDraftNoteRequest)
-// 		server := CreateRouter(client)
-// 		data := serveRequest(t, server, request, DraftNoteResponse{})
-//
-// 		assert(t, data.Message, "Draft note updated")
-// 		assert(t, data.Status, http.StatusOK)
-// 	})
-//
-// 	t.Run("Handles bad ID", func(t *testing.T) {
-// 		client := mock_main.NewMockClient(t)
-// 		mock_main.WithMr(t, client)
-// 		urlId := "abc"
-// 		client.EXPECT().UpdateDraftNote("", mock_main.MergeId, urlId, gomock.Any()).Return(&gitlab.DraftNote{}, makeResponse(http.StatusOK), nil)
-//
-// 		request := makeRequest(t, http.MethodPatch, fmt.Sprintf("/mr/draft_notes/%s", urlId), testUpdateDraftNoteRequest)
-// 		server := CreateRouter(client)
-// 		data := serveRequest(t, server, request, ErrorResponse{})
-//
-// 		assert(t, data.Message, "Could not parse draft note ID")
-// 		assert(t, data.Status, http.StatusBadRequest)
-// 	})
-//
-// 	t.Run("Handles empty note", func(t *testing.T) {
-// 		client := mock_main.NewMockClient(t)
-// 		mock_main.WithMr(t, client)
-// 		urlId := 10
-//
-// 		testEmptyUpdateDraftNoteRequest := testUpdateDraftNoteRequest
-// 		testEmptyUpdateDraftNoteRequest.Note = ""
-//
-// 		client.EXPECT().UpdateDraftNote("", mock_main.MergeId, urlId, gomock.Any()).Return(&gitlab.DraftNote{}, makeResponse(http.StatusOK), nil)
-//
-// 		request := makeRequest(t, http.MethodPatch, fmt.Sprintf("/mr/draft_notes/%d", urlId), testEmptyUpdateDraftNoteRequest)
-// 		server := CreateRouter(client)
-// 		data := serveRequest(t, server, request, ErrorResponse{})
-//
-// 		assert(t, data.Message, "Must provide draft note text")
-// 		assert(t, data.Status, http.StatusBadRequest)
-// 	})
-// }
-//
-// func TestPublishDraftNote(t *testing.T) {
-// 	t.Run("Should publish a draft note", func(t *testing.T) {
-// 		client := mock_main.NewMockClient(t)
-// 		mock_main.WithMr(t, client)
-// 		client.EXPECT().PublishDraftNote("", mock_main.MergeId, testDraftNotePublishRequest.Note).Return(makeResponse(http.StatusOK), nil)
-//
-// 		request := makeRequest(t, http.MethodPost, "/mr/draft_notes/publish", testDraftNotePublishRequest)
-// 		server := CreateRouter(client)
-//
-// 		data := serveRequest(t, server, request, SuccessResponse{})
-// 		assert(t, data.Message, "Draft note(s) published")
-// 		assert(t, data.Status, http.StatusOK)
-// 	})
-//
-// 	t.Run("Handles bad/missing ID", func(t *testing.T) {
-// 		client := mock_main.NewMockClient(t)
-// 		mock_main.WithMr(t, client)
-//
-// 		// Missing Note ID
-// 		testDraftNotePublishRequest := DraftNotePublishRequest{
-// 			PublishAll: false,
-// 		}
-//
-// 		request := makeRequest(t, http.MethodPost, "/mr/draft_notes/publish", testDraftNotePublishRequest)
-// 		server := CreateRouter(client)
-//
-// 		data := serveRequest(t, server, request, ErrorResponse{})
-// 		assert(t, data.Message, "Must provide Note ID")
-// 		assert(t, data.Status, http.StatusBadRequest)
-// 	})
-//
-// 	t.Run("Handles error", func(t *testing.T) {
-// 		client := mock_main.NewMockClient(t)
-// 		mock_main.WithMr(t, client)
-// 		client.EXPECT().PublishDraftNote("", mock_main.MergeId, testDraftNotePublishRequest.Note).Return(nil, errorFromGitlab)
-//
-// 		request := makeRequest(t, http.MethodPost, "/mr/draft_notes/publish", testDraftNotePublishRequest)
-// 		server := CreateRouter(client)
-// 		data := serveRequest(t, server, request, ErrorResponse{})
-//
-// 		assert(t, data.Message, "Could not publish draft note(s)")
-// 		assert(t, data.Status, http.StatusInternalServerError)
-// 	})
-// }
-//
-// func TestPublishAllDraftNotes(t *testing.T) {
-// 	t.Run("Should publish all draft notes", func(t *testing.T) {
-// 		client := mock_main.NewMockClient(t)
-// 		mock_main.WithMr(t, client)
-// 		client.EXPECT().PublishAllDraftNotes("", mock_main.MergeId).Return(makeResponse(http.StatusOK), nil)
-//
-// 		request := makeRequest(t, http.MethodPost, "/mr/draft_notes/publish", DraftNotePublishRequest{PublishAll: true})
-// 		server := CreateRouter(client)
-//
-// 		data := serveRequest(t, server, request, SuccessResponse{})
-// 		assert(t, data.Message, "Draft note(s) published")
-// 		assert(t, data.Status, http.StatusOK)
-// 	})
-//
-// 	t.Run("Should handle an error", func(t *testing.T) {
-// 		client := mock_main.NewMockClient(t)
-// 		mock_main.WithMr(t, client)
-// 		client.EXPECT().PublishAllDraftNotes("", mock_main.MergeId).Return(nil, errorFromGitlab)
-//
-// 		request := makeRequest(t, http.MethodPost, "/mr/draft_notes/publish", DraftNotePublishRequest{PublishAll: true})
-// 		server := CreateRouter(client)
-//
-// 		data := serveRequest(t, server, request, ErrorResponse{})
-// 		assert(t, data.Message, "Could not publish draft note(s)")
-// 		assert(t, data.Status, http.StatusInternalServerError)
-// 	})
-// }
