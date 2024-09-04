05dd3fbcf5fca3eeae9523f12937ecc2a393306a
Harrison Cramer <harrisoncramer@gmail.com>
2024-09-04 18:10:08 -0400

    Added reply test

--- cmd/app/reply_test.go
+++ cmd/app/reply_test.go
@@ -1,81 +1,45 @@
 package app
 
-// import (
-// 	"net/http"
-// 	"testing"
-//
-// 	"github.com/xanzy/go-gitlab"
-// 	mock_main "gitlab.com/harrisoncramer/gitlab.nvim/cmd/mocks"
-// 	"go.uber.org/mock/gomock"
-// )
-//
-// var testReplyRequest = ReplyRequest{
-// 	DiscussionId: "abc123",
-// 	Reply:        "Some Reply",
-// 	IsDraft:      false,
-// }
-//
-// func TestReplyHandler(t *testing.T) {
-// 	t.Run("Sends a reply", func(t *testing.T) {
-// 		client := mock_main.NewMockClient(t)
-// 		mock_main.WithMr(t, client)
-// 		client.EXPECT().AddMergeRequestDiscussionNote(
-// 			"",
-// 			mock_main.MergeId,
-// 			testReplyRequest.DiscussionId,
-// 			gomock.Any(),
-// 		).Return(&gitlab.Note{}, makeResponse(http.StatusOK), nil)
-//
-// 		request := makeRequest(t, http.MethodPost, "/mr/reply", testReplyRequest)
-// 		server := CreateRouter(client)
-//
-// 		data := serveRequest(t, server, request, ReplyResponse{})
-// 		assert(t, data.SuccessResponse.Message, "Replied to comment")
-// 		assert(t, data.SuccessResponse.Status, http.StatusOK)
-// 	})
-//
-// 	t.Run("Disallows non-POST methods", func(t *testing.T) {
-// 		client := mock_main.NewMockClient(t)
-// 		mock_main.WithMr(t, client)
-//
-// 		request := makeRequest(t, http.MethodPut, "/mr/reply", testReplyRequest)
-// 		server := CreateRouter(client)
-//
-// 		data := serveRequest(t, server, request, ErrorResponse{})
-// 		checkBadMethod(t, *data, http.MethodPost)
-// 	})
-//
-// 	t.Run("Handles errors from Gitlab client", func(t *testing.T) {
-// 		client := mock_main.NewMockClient(t)
-// 		mock_main.WithMr(t, client)
-// 		client.EXPECT().AddMergeRequestDiscussionNote(
-// 			"",
-// 			mock_main.MergeId,
-// 			testReplyRequest.DiscussionId,
-// 			gomock.Any(),
-// 		).Return(nil, nil, errorFromGitlab)
-//
-// 		request := makeRequest(t, http.MethodPost, "/mr/reply", testReplyRequest)
-// 		server := CreateRouter(client)
-//
-// 		data := serveRequest(t, server, request, ErrorResponse{})
-// 		checkErrorFromGitlab(t, *data, "Could not leave reply")
-// 	})
-//
-// 	t.Run("Handles non-200s from Gitlab client", func(t *testing.T) {
-// 		client := mock_main.NewMockClient(t)
-// 		mock_main.WithMr(t, client)
-// 		client.EXPECT().AddMergeRequestDiscussionNote(
-// 			"",
-// 			mock_main.MergeId,
-// 			testReplyRequest.DiscussionId,
-// 			gomock.Any(),
-// 		).Return(nil, makeResponse(http.StatusSeeOther), nil)
-//
-// 		request := makeRequest(t, http.MethodPost, "/mr/reply", testReplyRequest)
-// 		server := CreateRouter(client)
-//
-// 		data := serveRequest(t, server, request, ErrorResponse{})
-// 		checkNon200(t, *data, "Could not leave reply", "/mr/reply")
-// 	})
-// }
+import (
+	"net/http"
+	"testing"
+
+	"github.com/xanzy/go-gitlab"
+)
+
+type fakeReplyManager struct {
+	testBase
+}
+
+func (f fakeReplyManager) AddMergeRequestDiscussionNote(interface{}, int, string, *gitlab.AddMergeRequestDiscussionNoteOptions, ...gitlab.RequestOptionFunc) (*gitlab.Note, *gitlab.Response, error) {
+	resp, err := f.handleGitlabError()
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return &gitlab.Note{}, resp, err
+}
+
+func TestReplyHandler(t *testing.T) {
+	var testReplyRequest = ReplyRequest{DiscussionId: "abc123", Reply: "Some Reply", IsDraft: false}
+	t.Run("Sends a reply", func(t *testing.T) {
+		request := makeRequest(t, http.MethodPost, "/mr/reply", testReplyRequest)
+		svc := replyService{testProjectData, fakeReplyManager{}}
+		data := getSuccessData(t, svc, request)
+		assert(t, data.Message, "Replied to comment")
+		assert(t, data.Status, http.StatusOK)
+	})
+	t.Run("Handles errors from Gitlab client", func(t *testing.T) {
+		request := makeRequest(t, http.MethodPost, "/mr/reply", testReplyRequest)
+		svc := replyService{testProjectData, fakeReplyManager{testBase{errFromGitlab: true}}}
+		data := getFailData(t, svc, request)
+		checkErrorFromGitlab(t, data, "Could not leave reply")
+	})
+
+	t.Run("Handles non-200s from Gitlab client", func(t *testing.T) {
+		request := makeRequest(t, http.MethodPost, "/mr/reply", testReplyRequest)
+		svc := replyService{testProjectData, fakeReplyManager{testBase{status: http.StatusSeeOther}}}
+		data := getFailData(t, svc, request)
+		checkNon200(t, data, "Could not leave reply", "/mr/reply")
+	})
+}
