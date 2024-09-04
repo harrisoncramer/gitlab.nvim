b0918afe971d36a6e46b194c2357147b30164812
Harrison Cramer <harrisoncramer@gmail.com>
2024-09-04 17:02:00 -0400

    Updated job_test.go

--- cmd/app/job_test.go
+++ cmd/app/job_test.go
@@ -1,60 +1,71 @@
 package app
 
-// import (
-// 	"bytes"
-// 	"net/http"
-// 	"testing"
-//
-// 	mock_main "gitlab.com/harrisoncramer/gitlab.nvim/cmd/mocks"
-// )
-//
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/xanzy/go-gitlab"
+)
+
+type fakeTraceFileGetter struct {
+	testBase
+}
+
+func getTraceFileData(t *testing.T, svc ServiceWithHandler, request *http.Request) JobTraceResponse {
+	res := httptest.NewRecorder()
+	svc.handler(res, request)
+
+	var data JobTraceResponse
+	err := json.Unmarshal(res.Body.Bytes(), &data)
+	if err != nil {
+		t.Error(err)
+	}
+	return data
+}
+
+func (f fakeTraceFileGetter) GetTraceFile(pid interface{}, jobID int, options ...gitlab.RequestOptionFunc) (*bytes.Reader, *gitlab.Response, error) {
+	resp, err := f.handleGitlabError()
+	if err != nil {
+		return nil, nil, err
+	}
+	re := bytes.NewReader([]byte("Some data"))
+	return re, resp, err
+}
+
 // var jobId = 0
-//
-// func TestJobHandler(t *testing.T) {
-// 	t.Run("Should read a job trace file", func(t *testing.T) {
-// 		client := mock_main.NewMockClient(t)
-// 		client.EXPECT().GetTraceFile("", jobId).Return(bytes.NewReader([]byte("Some data")), makeResponse(http.StatusOK), nil)
-//
-// 		request := makeRequest(t, http.MethodGet, "/job", JobTraceRequest{})
-// 		// gitlabClient *Client, projectInfo *ProjectInfo, s ShutdownHandler, optFuncs ...optFunc
-// 		server := CreateRouter(client, &ProjectInfo{}, mockShutdownHandler{})
-// 		data := serveRequest(t, server, request, JobTraceResponse{})
-//
-// 		assert(t, data.SuccessResponse.Message, "Log file read")
-// 		assert(t, data.SuccessResponse.Status, http.StatusOK)
-// 		assert(t, data.File, "Some data")
-// 	})
-//
-// 	t.Run("Disallows non-GET methods", func(t *testing.T) {
-// 		client := mock_main.NewMockClient(t)
-// 		client.EXPECT().GetTraceFile("", jobId).Return(bytes.NewReader([]byte("Some data")), makeResponse(http.StatusOK), nil)
-//
-// 		request := makeRequest(t, http.MethodPost, "/job", JobTraceRequest{})
-// 		server := CreateRouter(client)
-// 		data := serveRequest(t, server, request, ErrorResponse{})
-//
-// 		checkBadMethod(t, *data, http.MethodGet)
-// 	})
-//
-// 	t.Run("Should handle errors from Gitlab", func(t *testing.T) {
-// 		client := mock_main.NewMockClient(t)
-// 		client.EXPECT().GetTraceFile("", jobId).Return(nil, nil, errorFromGitlab)
-//
-// 		request := makeRequest(t, http.MethodGet, "/job", JobTraceRequest{})
-// 		server := CreateRouter(client)
-// 		data := serveRequest(t, server, request, ErrorResponse{})
-//
-// 		checkErrorFromGitlab(t, *data, "Could not get trace file for job")
-// 	})
-//
-// 	t.Run("Should handle non-2jobIdjobIds", func(t *testing.T) {
-// 		client := mock_main.NewMockClient(t)
-// 		client.EXPECT().GetTraceFile("", jobId).Return(nil, makeResponse(http.StatusSeeOther), nil)
-//
-// 		request := makeRequest(t, http.MethodGet, "/job", JobTraceRequest{})
-// 		server := CreateRouter(client)
-// 		data := serveRequest(t, server, request, ErrorResponse{})
-//
-// 		checkNon200(t, *data, "Could not get trace file for job", "/job")
-// 	})
-// }
+func TestJobHandler(t *testing.T) {
+	t.Run("Should read a job trace file", func(t *testing.T) {
+		request := makeRequest(t, http.MethodGet, "/job", JobTraceRequest{})
+		client := fakeTraceFileGetter{}
+		svc := traceFileService{emptyProjectData, client}
+		data := getTraceFileData(t, svc, request)
+		assert(t, data.Message, "Log file read")
+		assert(t, data.Status, http.StatusOK)
+		assert(t, data.File, "Some data")
+	})
+	t.Run("Disallows non-GET methods", func(t *testing.T) {
+		request := makeRequest(t, http.MethodPost, "/job", JobTraceRequest{})
+		client := fakeTraceFileGetter{}
+		svc := traceFileService{emptyProjectData, client}
+		data := getFailData(t, svc, request)
+		checkBadMethod(t, data, http.MethodGet)
+	})
+	t.Run("Handles errors from Gitlab client", func(t *testing.T) {
+		request := makeRequest(t, http.MethodGet, "/job", JobTraceRequest{})
+		client := fakeTraceFileGetter{testBase{errFromGitlab: true}}
+		svc := traceFileService{emptyProjectData, client}
+		data := getFailData(t, svc, request)
+		checkErrorFromGitlab(t, data, "Could not get trace file for job")
+	})
+
+	t.Run("Handles non-200s from Gitlab client", func(t *testing.T) {
+		request := makeRequest(t, http.MethodGet, "/job", JobTraceRequest{})
+		client := fakeTraceFileGetter{testBase{status: http.StatusSeeOther}}
+		svc := traceFileService{emptyProjectData, client}
+		data := getFailData(t, svc, request)
+		checkNon200(t, data, "Could not get trace file for job", "/job")
+	})
+}
