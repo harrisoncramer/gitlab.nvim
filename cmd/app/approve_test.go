47a0a75e72e2d07a155da05dd59ac637563d6804
Harrison Cramer <harrisoncramer@gmail.com>
2024-09-04 12:46:28 -0400

    Updated cmd/app/approve_test.go

--- cmd/app/approve_test.go
+++ cmd/app/approve_test.go
@@ -7,10 +7,19 @@ import (
 	"github.com/xanzy/go-gitlab"
 )
 
-type fakeApprover struct{}
+type fakeApprover struct {
+	errFromGitlab bool
+	status        int
+}
 
 func (f fakeApprover) ApproveMergeRequest(pid interface{}, mr int, opt *gitlab.ApproveMergeRequestOptions, options ...gitlab.RequestOptionFunc) (*gitlab.MergeRequestApprovals, *gitlab.Response, error) {
-	return &gitlab.MergeRequestApprovals{}, makeResponse(http.StatusOK), nil
+	if f.errFromGitlab {
+		return nil, nil, errorFromGitlab
+	}
+	if f.status == 0 {
+		f.status = 200
+	}
+	return &gitlab.MergeRequestApprovals{}, makeResponse(f.status), nil
 }
 
 func TestApproveHandler(t *testing.T) {
@@ -23,38 +32,27 @@ func TestApproveHandler(t *testing.T) {
 		assert(t, data.Status, http.StatusOK)
 	})
 
-	// t.Run("Disallows non-POST method", func(t *testing.T) {
-	// 	client := mocks.NewMockClient(t)
-	// 	mocks.WithMr(t, client)
-	// 	client.EXPECT().ApproveMergeRequest("", mocks.MergeId, nil, nil).Return(&gitlab.MergeRequestApprovals{}, makeResponse(http.StatusOK), nil)
-	//
-	// 	request := makeRequest(t, http.MethodPut, "/mr/approve", nil)
-	// 	server := CreateRouter(client)
-	// 	data := serveRequest(t, server, request, ErrorResponse{})
-	// 	checkBadMethod(t, *data, http.MethodPost)
-	// })
-	//
-	// t.Run("Handles errors from Gitlab client", func(t *testing.T) {
-	// 	client := mocks.NewMockClient(t)
-	// 	mocks.WithMr(t, client)
-	// 	client.EXPECT().ApproveMergeRequest("", mocks.MergeId, nil, nil).Return(nil, nil, errorFromGitlab)
-	//
-	// 	request := makeRequest(t, http.MethodPost, "/mr/approve", nil)
-	// 	server := CreateRouter(client)
-	// 	data := serveRequest(t, server, request, ErrorResponse{})
-	//
-	// 	checkErrorFromGitlab(t, *data, "Could not approve merge request")
-	// })
-	//
-	// t.Run("Handles non-200s from Gitlab client", func(t *testing.T) {
-	// 	client := mocks.NewMockClient(t)
-	// 	mocks.WithMr(t, client)
-	// 	client.EXPECT().ApproveMergeRequest("", mocks.MergeId, nil, nil).Return(nil, makeResponse(http.StatusSeeOther), nil)
-	//
-	// 	request := makeRequest(t, http.MethodPost, "/mr/approve", nil)
-	// 	server := CreateRouter(client)
-	// 	data := serveRequest(t, server, request, ErrorResponse{})
-	//
-	// 	checkNon200(t, *data, "Could not approve merge request", "/mr/approve")
-	// })
+	t.Run("Disallows non-POST method", func(t *testing.T) {
+		request := makeRequest(t, http.MethodGet, "/mr/approve", nil)
+		client := fakeApprover{}
+		svc := mergeRequestApproverService{emptyProjectData, client}
+		data := getFailData(t, svc, request)
+		checkBadMethod(t, data, http.MethodPost)
+	})
+
+	t.Run("Handles errors from Gitlab client", func(t *testing.T) {
+		request := makeRequest(t, http.MethodPost, "/mr/approve", nil)
+		client := fakeApprover{errFromGitlab: true}
+		svc := mergeRequestApproverService{emptyProjectData, client}
+		data := getFailData(t, svc, request)
+		checkErrorFromGitlab(t, data, "Could not approve merge request")
+	})
+
+	t.Run("Handles non-200s from Gitlab client", func(t *testing.T) {
+		request := makeRequest(t, http.MethodPost, "/mr/approve", nil)
+		client := fakeApprover{status: http.StatusSeeOther}
+		svc := mergeRequestApproverService{emptyProjectData, client}
+		data := getFailData(t, svc, request)
+		checkNon200(t, data, "Could not approve merge request", "/mr/approve")
+	})
 }
