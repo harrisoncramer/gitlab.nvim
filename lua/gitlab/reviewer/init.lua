ecec29c5d0871de3b79ae0d67d92e27228f731e8
Jakub F. Bortlík <jakub.bortlik@proton.me>
2024-09-19 17:28:03 +0200

    Feat: Redraw screen with cursor at center

--- lua/gitlab/reviewer/init.lua
+++ lua/gitlab/reviewer/init.lua
@@ -146,6 +146,7 @@ M.jump = function(file_name, line_number, new_buffer)
   end
   vim.api.nvim_win_set_cursor(0, { line_number, 0 })
   u.open_fold_under_cursor()
+  vim.cmd("normal! zz")
 end
 
 ---Get the data from diffview, such as line information and file name. May be used by
