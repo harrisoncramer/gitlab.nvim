1eb187bb3a412206b2a0e7a344d093556686db79
Jakub F. Bortlík <jakub.bortlik@proton.me>
2024-11-25 10:56:16 +0100

    perf: only check tree cleanness with imply_local

--- lua/gitlab/reviewer/init.lua
+++ lua/gitlab/reviewer/init.lua
@@ -42,11 +42,12 @@ M.open = function()
   end
 
   local diffview_open_command = "DiffviewOpen"
-  local has_clean_tree, err = git.has_clean_tree()
-  if err ~= nil then
-    return
-  end
+
   if state.settings.reviewer_settings.diffview.imply_local then
+    local has_clean_tree, err = git.has_clean_tree()
+    if err ~= nil then
+      return
+    end
     if has_clean_tree then
       diffview_open_command = diffview_open_command .. " --imply-local"
     else
