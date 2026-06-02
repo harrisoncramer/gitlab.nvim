e210b52d945e1f73329eb74b7c0a5f34c786c813
Jakub F. Bortlík <jakub.bortlik@proton.me>
2026-06-02 23:01:44 +0200

    feat: pull emoji data from Gitlab
    Instead of using an incomplete local emojis.json list, the plugin now
    pulls the emoji data from Gitlab. This makes sure the data is complete
    and doesn't contain invalid items, which would cause a Go server error
    (e.g., cmd/config/emojis.json contained :boat: for :sailboat:).

--- lua/gitlab/init.lua
+++ lua/gitlab/init.lua
@@ -45,6 +45,7 @@ local function setup(args)
   end
 
   state.merge_settings(args) -- Merges user settings with default settings
+  state.set_plugin_configuration() -- Sets auth_token and gitlab_url into the settings module
   server.build() -- Builds the Go binary if it doesn't exist
   state.set_global_keymaps() -- Sets keymaps that are not bound to a specific buffer
   require("gitlab.colors") -- Sets colors
