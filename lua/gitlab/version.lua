46a49be1f90a1fe5f8184d2bfbe0b4d15966a1d8
Artem Starikov <contact@snejugal.ru>
2026-07-13 11:28:20 +0300

    Move required Go version to a variable

--- lua/gitlab/version.lua
+++ lua/gitlab/version.lua
@@ -1,5 +1,7 @@
 local M = {}
 
+local minimum_go_version = "1.25.1"
+
 M.is_go_valid = function()
   local has_go, go = pcall(vim.system, { "go", "version" })
   if not has_go then
@@ -7,13 +9,16 @@ M.is_go_valid = function()
   end
 
   local go_version = vim.version.parse(go:wait().stdout, { strict = false })
-  return go_version ~= nil and vim.version.ge(go_version, { 1, 25, 0 })
+  return go_version ~= nil and vim.version.ge(go_version, minimum_go_version)
 end
 
 M.check_go_version = function()
   local has_version = M.is_go_valid()
   if not has_version then
-    return "Go is not installed, or version is older than 1.25.1. Please reinstall up-to-date Go version: https://go.dev/dl/"
+    return string.format(
+      "Go is not installed, or version is older than %s. Please reinstall up-to-date Go version: https://go.dev/dl/",
+      minimum_go_version
+    )
   end
 end
 
