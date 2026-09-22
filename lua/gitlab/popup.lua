f8a9aefbbfc54e82cf86dd8afafadd0d55c8a6ea
Jakub Bortlík <jakub.bortlik@proton.me>
2026-09-22 10:38:45 +0200

    fix: merge popup opts correctly

--- lua/gitlab/popup.lua
+++ lua/gitlab/popup.lua
@@ -6,7 +6,7 @@ local M = {}
 ---@param opts PopupOpts Options for customizing the popup
 ---@return table
 M.create_popup_state = function(opts)
-  opts = opts or { filetype = "markdown" }
+  opts = u.merge({ filetype = "markdown" }, opts)
   local settings = u.merge(require("gitlab.state").settings.popup, opts.user_settings or {})
   local view_opts = {
     buf_options = {
